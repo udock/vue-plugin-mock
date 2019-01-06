6df07bf4d760c18a56b8cda379b60bc2992c8bc5
vaniship <vaniship@gmail.com>
2019-01-06 17:00:00 +0800

    feat: make reuse

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,19 +1,6 @@
 module.exports = {
-  external: (id) => /^(path|axios|wurl|mockjs|lodash\/.*)$/.test(id),
+  external: (id) => /^(@udock\/plugin-mock)$/.test(id),
   globals: {
-    'axios': 'axios',
-    'babel-runtime/core-js/promise': 'core.Promise',
-    'babel-runtime/core-js/get-iterator': 'core.getIterator',
-    'babel-runtime/core-js/json/stringify': 'core.JSON.stringify',
-    'lodash/defaults': '_.defaults',
-    'lodash/extend': '_.extend',
-    'lodash/has': '_.has',
-    'lodash/isObject': '_.isObject',
-    'lodash/mapValues': '_.mapValues',
-    'lodash/merge': '_.merge',
-    'lodash/pickBy': '_.pickBy',
-    'mockjs': 'Mock',
-    'path': 'path',
-    'wurl': 'wurl'
+    '@udock/plugin-mock': 'udock.plugin.mock'
   }
 }
